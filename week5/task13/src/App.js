80f0020bf241a5d539e13253f46898644572e879
Rakes-kumar06 <rakeshkumarsofficial@gmail.com>
2026-07-21 20:17:22 +0530

    Week5 React tasks have been completed

--- week5/task13/src/App.js
+++ week5/task13/src/App.js
@@ -1,23 +1,68 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { books, courses, blogs } from './Data';
 
 function App() {
+  const showBooks = true;
+  const showBlogs = true;
+  const showCourses = true;
+
+  let bookdet = null;
+  if (showBooks) {
+    bookdet = (
+      <ul>
+        {}
+        {books.map((book) =>
+          <div key={book.id}>
+            <h3>{book.bname}</h3>
+            <h4>{book.price}</h4>
+          </div>
+        )}
+      </ul>
+    );
+  }
+
+  const content = showBlogs && (
+    <ul>
+      {blogs.map((blog) =>
+        <div key={blog.id}>
+          <h2>{blog.title}</h2>
+          <h4>{blog.author}</h4>
+          <p>{blog.snippet}</p>
+        </div>
+      )}
+    </ul>
+  );
+
+  const coursedet = showCourses ? (
+    <ul>
+      {courses.map((course) =>
+        <div key={course.id}>
+          <h2>{course.name}</h2>
+          <h4>{course.date}</h4>
+        </div>
+      )}
+    </ul>
+  ) : <p>No courses available</p>;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React 
-        </a>
-      </header>
+    <div className="container">
+        
+      <div className="mystyle1">
+        <h1>Course Details</h1>
+        {coursedet}
+      </div>
+
+      <div className="v1">
+        <h1>Book Details</h1>
+        {bookdet}
+      </div>
+
+      <div className="st2">
+        <h1>Blog Details</h1>
+        {content}
+      </div>
+
     </div>
   );
 }
